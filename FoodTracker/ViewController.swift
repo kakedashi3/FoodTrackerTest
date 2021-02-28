22252d0073b58651a48136c462faf1a5e9e04b76
ぱぶろう <69132449+Pabulow2060@users.noreply.github.com>
2021-02-28 23:47:39 +0900

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by 立岡力 on 2021/02/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
