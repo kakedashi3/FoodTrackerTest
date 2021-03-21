7ee932736b38b1811aa1fceb3d80867cceaf076c
tatsuoka <69132449+Pabulow2060@users.noreply.github.com>
2021-03-22 06:15:27 +0900

    2021/03/22

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -11,41 +11,38 @@ class MealTableViewController: UITableViewController {
 
     //MARK: Properties
     
-    var meal = [Meal]()
+    var meals = [Meal]()
     
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        //Load the sample date.
+        loadSampleMeals()
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return meals.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "MealTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        }
 
         // Configure the cell...
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -97,6 +94,23 @@ class MealTableViewController: UITableViewController {
     
     private func loadSampleMeals() {
         
+        let photo1 = UIImage(named: "meal1")
+        let photo2 = UIImage(named: "meal2")
+        let photo3 = UIImage(named: "meal3")
+        
+        guard let meal1 = Meal(name:"Caprese Salad", photo: photo1, rating: 4)
+            else { fatalError("Unable to instantiate meal1")
+            }
+        
+        guard let meal2 = Meal(name:"Chicken and Potatoes", photo: photo2, rating: 5)
+            else { fatalError("Unable to instantiate meal2")
+            }
+        
+        guard let meal3 = Meal(name:"Pasta with Meatballs", photo: photo3, rating: 3)
+            else { fatalError("Unable to instantiate meal3")
+            }
+        
+        meals += [meal1, meal2, meal3]
     }
     
 
